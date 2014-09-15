3ec8b7130bf958d20b9a4af66b35d3ff4f61304f
KaiserEMP <kaidogta3@hotmail.com>
2014-09-15 14:51:23 +0300

    added comments and function view post

--- controllers/posts.php
+++ controllers/posts.php
@@ -21,10 +21,17 @@ class posts extends Controller{
         echo "\$_POST:<br>";
         var_dump($_POST);
     }
+    function view_post(){
+        $data = $_POST['data'];
+       $data['post_id'] = $this->params[0];
+       $data['comment_author'] = 'yolooo';
+      insert('comment', $data);
+}
     function view(){
         $post_id = $this->params[0];
         $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
         $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+
     }
 
 }
