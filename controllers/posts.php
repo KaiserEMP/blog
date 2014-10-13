3bc9aa619e7a60140bc12123cf4e6b2b65341354
KaiserEMP <kaidogta3@hotmail.com>
2014-10-13 13:15:14 +0300

    Add heading to tag detail view Add user management Update database schema Fix tags in post detail view Linkify posts in tags page Add heading to a tag details page Fixed different comments for each post

--- controllers/posts.php
+++ controllers/posts.php
@@ -28,10 +28,14 @@ class posts extends Controller{
       insert('comment', $data);
 }
     function view(){
-        $post_id = $this->params[0];
-        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
-        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
-        $this->comments = get_all("SELECT * FROM comment");
+        $post_id=$this->params[0];
+        $this->post=get_first("SELECT*FROM post
+                                NATURAL JOIN user
+                                WHERE post_id='$post_id'");
+        $this->tags = get_all("SELECT*FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+        $this->comments = get_all("SELECT*FROM comment
+                                    NATURAL JOIN post
+                                    WHERE post_id='$post_id'");
     }
 
 }
